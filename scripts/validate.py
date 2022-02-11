57a0e92ccf9c0d7b032a20ff91ccad6c552bc015
Oleksiy Syvokon <oleksiy.syvokon@gmail.com>
2022-02-12 00:44:45 +0200

    Check files with missing detailed annotations

--- scripts/validate.py
+++ scripts/validate.py
@@ -23,11 +23,23 @@ def check_files_without_annotations(corpus):
     return not missing_by_partition
 
 
+def check_files_with_missing_detailed_annotations(corpus):
+    reannotate = set()
+    for doc in corpus:
+        for ann in doc.annotated.get_annotations():
+            if ann.meta["error_type"] in ("Grammar", "Fluency"):
+                reannotate.add(doc.doc_id)
+
+    if reannotate:
+        print(f"{len(reannotate)} docs with missing detailed annotations:")
+        print("\n".join(sorted(reannotate)))
+
 
 def main():
     corpus = Corpus("all")
 
-    check_files_without_annotations(corpus)
+    #check_files_without_annotations(corpus)
+    check_files_with_missing_detailed_annotations(corpus)
 
 if __name__ == "__main__":
     main()
