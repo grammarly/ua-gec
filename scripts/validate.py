ccadc7fcb4ec2deb29e6d3abf6296d7484669040
Oleksiy Syvokon <oleksiy.syvokon@gmail.com>
2022-02-20 18:44:32 +0200

    Exceptions list for the missing annotations check

--- scripts/validate.py
+++ scripts/validate.py
@@ -9,8 +9,9 @@ from ua_gec import Corpus
 
 def check_files_without_annotations(corpus):
     missing_by_partition = defaultdict(list)
+    exceptions = ["0460", "0650", "1875", "1880"]
     for doc in corpus:
-        if not doc.annotated.get_annotations():
+        if not doc.annotated.get_annotations() and not doc.doc_id in exceptions:
             part = doc.meta.partition
             missing_by_partition[part].append(doc.doc_id)
 
@@ -32,13 +33,13 @@ def check_files_with_missing_detailed_annotations(corpus):
 
     if reannotate:
         print(f"{len(reannotate)} docs with missing detailed annotations:")
-        print("\n".join(sorted(reannotate)))
+        #print("\n".join(sorted(reannotate)))
 
 
 def main():
     corpus = Corpus("all")
 
-    #check_files_without_annotations(corpus)
+    check_files_without_annotations(corpus)
     check_files_with_missing_detailed_annotations(corpus)
 
 if __name__ == "__main__":
