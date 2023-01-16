5b3ed00c2126c20c4ba58fe7508b50763ebaeffe
Oleksiy Syvokon <oleksiy.syvokon@gmail.com>
2023-01-16 17:15:57 +0200

    Check that the number of source and target sentences match

--- scripts/validate.py
+++ scripts/validate.py
@@ -70,6 +70,24 @@ def check_gec_only_and_gec_fluency_source_match():
         print(', '.join(sorted(broken)))
 
 
+def check_number_of_source_and_target_sentences():
+    """Check that the number of source and target sentences match. """
+
+    for layer in (AnnotationLayer.GecOnly, AnnotationLayer.GecAndFluency):
+        corpus = Corpus("all", annotation_layer=layer)
+        broken = []
+        for doc in corpus:
+            msg = f"{doc.doc_id}.annotator_id={doc.meta.annotator_id} ({layer})"
+            if len(doc.source_sentences) != len(doc.target_sentences):
+                broken.append(msg)
+            if len(doc.source_sentences_tokenized) != len(doc.target_sentences_tokenized):
+                broken.append(msg)
+
+        if broken:
+            print(f"{len(broken)} docs have different number of source and target sentences:")
+            print(', '.join(sorted(broken)))
+
+
 def main():
     corpus = Corpus("all")
 
@@ -77,6 +95,7 @@ def main():
     check_files_with_missing_detailed_annotations(corpus)
     check_double_annotated(corpus)
     check_gec_only_and_gec_fluency_source_match()
+    check_number_of_source_and_target_sentences()
 
 if __name__ == "__main__":
     main()
